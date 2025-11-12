c91b634da98310ebceffff3c82b952f35e3c1f5f
kjb1045 <kjb1045@naver.com>
2025-11-13 04:26:09 +0900

    2025-11-13배포

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
-import { createContext, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createContext, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from './header/Header';
 import Section from './section/Section';
 import Footer from './footer/Footer';
 import LoginPages from './header/LoginPages';
 
-
 function App() {
-
   return (
     // <>
-      
-      <BrowserRouter>
-        <Header/>
-        <Routes>
-            <Route path='/' element={<Section/>}/>
-            <Route path='/LoginPages' element={<LoginPages/>}/>
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
+
+    <HashRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Section />} />
+        <Route path="/LoginPages" element={<LoginPages />} />
+      </Routes>
+      <Footer />
+    </HashRouter>
     // {/* </> */}
-  )
+  );
 }
 
-export default App
+export default App;
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,35 +1,56 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import LoginPages from "./LoginPages";
-import Section from "../section/Section";
-
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import LoginPages from './LoginPages';
+import Section from '../section/Section';
+import tivingLogo from '../../img/1695032536_1.svg';
+import backimg from '../../img/6u5x4l6u5x4l6u5x.png';
 import './Header.css';
 
-export default function Header(){
-
-    return (
-        <>
-            <div className="header-container">
-                <div className="top-box">
-                    <ul className="tbox01">
-                        <li><Link to='/'><img src="img/1695032536_1.svg" alt="tving logo" /></Link></li>
-                        <li><Link to='/Section'>드라마</Link></li>
-                        <li><Link to='/'>예능</Link></li>
-                        <li><Link to='/'>영화</Link></li>
-                        <li><Link to='/'>스포츠</Link></li>
-                        <li><Link to='/'>애니</Link></li>
-                        <li><Link to='/'>뉴스</Link></li>
-                    </ul>
-                    <ul className="tbox02">
-                        <li><input type="text" placeholder="작품명을 입력하세요."/>🔍</li>
-                        <li><Link to='/LoginPages'>로그인</Link></li>
-                        {/* <li><img src="img/vpace.jpg" alt="" /></li> */}
-                    </ul>
-                </div>
-                <div className="centerbox">
-                    <img src="img/6u5x4l6u5x4l6u5x.png" alt="poster" />
-                </div>
-                {/* <div className="bottombox">
+export default function Header() {
+  return (
+    <>
+      <div className="header-container">
+        <div className="top-box">
+          <ul className="tbox01">
+            <li>
+              <Link to="/">
+                <img src={tivingLogo} alt="tving logo" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/Section">드라마</Link>
+            </li>
+            <li>
+              <Link to="/">예능</Link>
+            </li>
+            <li>
+              <Link to="/">영화</Link>
+            </li>
+            <li>
+              <Link to="/">스포츠</Link>
+            </li>
+            <li>
+              <Link to="/">애니</Link>
+            </li>
+            <li>
+              <Link to="/">뉴스</Link>
+            </li>
+          </ul>
+          <ul className="tbox02">
+            <li>
+              <input type="text" placeholder="작품명을 입력하세요." />
+              🔍
+            </li>
+            <li>
+              <Link to="/LoginPages">로그인</Link>
+            </li>
+            {/* <li><img src="img/vpace.jpg" alt="" /></li> */}
+          </ul>
+        </div>
+        <div className="centerbox">
+          <img src={backimg} alt="poster" />
+        </div>
+        {/* <div className="bottombox">
                         <TopMenu/>
                     <button type="button"><Link to=''>드라마</Link></button>
                     <button type="button"><Link to=''>로맨스</Link></button>
@@ -44,7 +65,7 @@ export default function Header(){
                     <button type="button"><Link to=''>공포(호러)</Link></button>
                     <button type="button"><Link to=''>다큐멘터리</Link></button>
                 </div> */}
-            </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
